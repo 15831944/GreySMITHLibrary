1472d6df5c4b53af3aae86b64bdac9ddc8e97032
smith.preston2@gmail.com <Lightly5>
2015-04-30 09:53:39 -0400

    excluded extra references

--- Utilities/GS_Autodesk/Revit/Extensions/Parameters/ParameterUtils.cs
+++ Utilities/GS_Autodesk/Revit/Extensions/Parameters/ParameterUtils.cs
@@ -40,65 +40,6 @@ namespace GreySMITH.Utilities.GS_Autodesk.Revit.Extensions.Parameters
         }
     }
 
-    #region Experimental Code
-
-    public interface IRoll
-    {
-        void roll();
-    }
-
-    abstract class CircularObject
-    {
-        int diameter;
-        public double volume()
-        {
-            double p = diameter * 3.14;
-
-            return p;
-        }
-
-        virtual void printperimeter()
-        {
-            Console.WriteLine("The volume of this object is incorrect because it doesn't take depth into account");
-        }
-    }
-
-    class Wheel : CircularObject, IRoll
-    {
-        public void roll()
-        {
-            throw new NotImplementedException();
-        }
-
-        override void printperimeter()
-        {
-            Console.WriteLine("the volume of this object...");
-        }
-    }
-
-    
-
-#endregion
-
-    //Binding binding = 
-
-
-
-
-    //DefinitionBindingMapIterator dbmi = bm.ForwardIterator();
-    //int bindingmapsize = bm.Size;
-    //Debug.Print("testing");
-
-    //string n = "efdsfdsf";
-
-
-    //while(dbmi.MoveNext())
-    //{
-    //    Definition def = dbmi.Key as Definition;
-    //    Binding bin = dbmi.Current as Binding;
-    //}
-
-
     ///* Parameter is a concrete class
     // * of the abstract class DEFINITION
     // * all of the info we want about the parameter comes from
@@ -110,13 +51,4 @@ namespace GreySMITH.Utilities.GS_Autodesk.Revit.Extensions.Parameters
 
     //// kind of parameter (i.e.: Yes|No, Volume, Horsepower
     //ParameterType param_kind = d.ParameterType;
-
-    //// 
-
-
-
-
-    //Element e = param.Element;
-
-    //BindingMap
 }
